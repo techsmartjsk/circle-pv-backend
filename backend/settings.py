d8bf5857de18f08a97e1902168dfacf84279fae6
Jaskirat Singh <techsmartjsk@gmail.com>
2025-10-22 01:12:07 +0530

    deployment ready

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
-# STATIC_URL = "static/"
+STATIC_URL = "static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
