01d643757ebf7985a9587c27e343da985032a766
Jaskirat Singh <techsmartjsk@gmail.com>
2025-10-22 02:34:11 +0530

    deployment ready

--- backend/settings.py
+++ backend/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/staticfiles")]
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
